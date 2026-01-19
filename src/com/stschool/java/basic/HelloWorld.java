60b045dac33e890d011ba74c289f9f48e38c55ad
maheshporla <maheshporla93@gmail.com>
2026-01-19 16:21:54 +0530

    initial commit

---
+++ src/com/stschool/java/basic/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.stschool.java.basic;
+
+import java.sql.SQLOutput;
+import java.util.SortedSet;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+    }
+}
