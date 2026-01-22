390a981094ca9332ee7707f90ad60244a9ef61cb
maheshporla <maheshporla93@gmail.com>
2026-01-22 11:32:39 +0530

    my data variables

--- src/com/stschool/java/basics/Primitives.java
+++ src/com/stschool/java/basics/Primitives.java
@@ -2,13 +2,21 @@ package com.stschool.java.basics;
 
 public class Primitives {
     public static void main(String[] args){
-        byte age = 127;
-        short year = 2026 ;
-        int salary = 300000;
+        byte age ,interPassMarks ;
+        age= 127;
+        interPassMarks=35;
+        short year,interMarks,bagPrice;
+        year= 2026 ;
+        interMarks=1000;
+        bagPrice=800;
+        int salary ,vehicle = 300000;
         long network = 897065424132L;
+        float temp ,height = 36.5F ;
+        double bloodPressure = 100.264D ;
         System.out.println("Age : " + age);
         System.out.println("Year : " + year);
-        System.out.println("Salary : " + salary);
+
         System.out.println("Network : " + network);
+        System.out.println("");
     }
 }
