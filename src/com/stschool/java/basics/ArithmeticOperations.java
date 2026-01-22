41e43cd95766365ada39fa8accc315ec2a0c9c10
maheshporla <maheshporla93@gmail.com>
2026-01-22 21:22:09 +0530

    ArithmeticOperations

---
+++ src/com/stschool/java/basics/ArithmeticOperations.java
@@ -0,0 +1,20 @@
+package com.stschool.java.basics;
+
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+        public static void main (String[] args){
+            //Scanner sc= new Scanner(System.in);
+            byte no1, no2;
+            no1=127;
+            no2=127;
+            short res;
+            //int res;
+            //double res;
+            res = (short) (no1+no2);
+            System.out.println(res);
+
+        }
+    }
+
+
